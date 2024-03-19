e08fb6114158b4e36870ccf24045e25fd56a2c3d
ghazalbaghdadi <baghdadighazal65@gmail.com>
2024-03-19 18:33:10 +0330

    first commit !

---
+++ Booster-Js-5/3-Game password.js
@@ -0,0 +1,26 @@
+/*
+- We have three inputs to enter the password.
+
+- Passwords must be randomly generated.
+
+- We provide 5 hints for the user, which should be as follows:
+  1. One Number is correct and well placed.
+  2. One Number is correct but wrong placed.
+  3. Two numbers are correct but wrong placed.
+  4. Nothing is Correct.
+  5. One Number is correct but wrong placed.
+
+- If the user guesses correctly the first time, he must get 10 points.
+
+- If the user guesses correctly in the second attempt, he gets 5 points.
+
+- After the user guesses correctly, a new password must be generated for the user to guess.
+* The score of each stage is added to the previous stages.
+
+- If the user does not find the password after 60 seconds or after guessing twice, a 'Game Over 😥' message should be displayed.
+
+- After the game is finished, the list of the user's games should be shown along with his points.
+
+- The highest score should be shown in gold color.
+* Refer to Game "password.png" photo for more guidance.
+*/
